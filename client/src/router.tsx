55f030c42422628796fc4d5593e919da8bc50755
Afzalkhan377 <97460290+Afzalkhan377@users.noreply.github.com>
2024-04-22 16:53:54 -0400

    fixing

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
 import AI from "./pages/AI";
+import Spotify from "./pages/Spotify";
 const Router = () => {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ const Router = () => {
        <Route path = "/" element={<Home />} />
        <Route path = "/register" element={<Registration />} />
        <Route path = "/ai" element={<AI/>} />
+       <Route path = "/spotify-auth" element={<Spotify />} />
       </Routes>
     </BrowserRouter>
   );
